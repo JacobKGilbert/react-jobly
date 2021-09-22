738742b7a24dd9fd6748558b655e86c152ab8ab2
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-22 12:40:35 -0500

    added function for collecting job data

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -12,25 +12,23 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class JoblyApi {
   // the token for interacting with the API will be stored here.
-  static token;
+  static token
 
-  static async request(endpoint, data = {}, method = "get") {
-    console.debug("API Call:", endpoint, data, method);
+  static async request(endpoint, data = {}, method = 'get') {
+    console.debug('API Call:', endpoint, data, method)
 
     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
-    const url = `${BASE_URL}/${endpoint}`;
-    const headers = { Authorization: `Bearer ${JoblyApi.token}` };
-    const params = (method === "get")
-        ? data
-        : {};
+    const url = `${BASE_URL}/${endpoint}`
+    const headers = { Authorization: `Bearer ${JoblyApi.token}` }
+    const params = method === 'get' ? data : {}
 
     try {
-      return (await axios({ url, method, data, params, headers })).data;
+      return (await axios({ url, method, data, params, headers })).data
     } catch (err) {
-      console.error("API Error:", err.response);
-      let message = err.response.data.error.message;
-      throw Array.isArray(message) ? message : [message];
+      console.error('API Error:', err.response)
+      let message = err.response.data.error.message
+      throw Array.isArray(message) ? message : [message]
     }
   }
 
@@ -39,13 +37,18 @@ class JoblyApi {
   /** Get details on a company by handle. */
 
   static async getCompany(handle) {
-    let res = await this.request(`companies/${handle}`);
-    return res.company;
+    let res = await this.request(`companies/${handle}`)
+    return res.company
   }
 
   static async getAllCompanies(data) {
     let res = await this.request(`companies/`, data)
     return res.companies
+  }
+
+  static async getAllJobs(data) {
+    let res = await this.request(`jobs/`, data)
+    return res.jobs
   }
 
   // obviously, you'll add a lot here ...
