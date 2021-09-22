7a7b2a1cfeaaf56f98938a4a3554eef533075fbe
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-22 17:03:19 -0500

    created form

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -1,10 +1,47 @@
-import React from 'react'
-import { Form } from 'reactstrap';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { Button, Form, Input, Label } from 'reactstrap';
 
-const LoginForm = () => {
+const LoginForm = ({ login }) => {
+  const INITIAL_STATE = {
+    username: '',
+    password: ''
+  }
+  const [formData, setFormData] = useState(INITIAL_STATE)
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target
+    setFormData((fData) => ({
+      ...fData,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    login({ ...formData })
+    setFormData(INITIAL_STATE)
+  }
+
   return(
-    <Form>
-      
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor='username'>Username</Label>
+      <Input 
+        type='text' 
+        id='username' 
+        name='username' 
+        onChange={handleChange} 
+      />
+      <Label htmlFor='password'>Password</Label>
+      <Input 
+        type='password' 
+        id='password' 
+        name='password' 
+        onChange={handleChange} 
+      />
+      <Button color='primary'>Log In</Button>
+      <p>Not already a user?</p>
+      <Link to='/signup'><Button outline color='secondary'>Sign Up</Button></Link>
     </Form>
   )
 }
--- frontend/src/SignUpForm.js
+++ frontend/src/SignUpForm.js
@@ -1,10 +1,67 @@
-import React from 'react'
-import { Form } from 'reactstrap'
+import React, { useState } from 'react'
+import { Form, Label, Input, Button } from 'reactstrap'
 
-const SignUpForm = () => {
+const SignUpForm = ({ signup }) => {
+  const INITIAL_STATE = {
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+  }
+  const [formData, setFormData] = useState(INITIAL_STATE)
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target
+    setFormData((fData) => ({
+      ...fData,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    signup({ ...formData })
+    setFormData(INITIAL_STATE)
+  }
   return (
-    <Form>
-      
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor="username">Username</Label>
+      <Input
+        type="text"
+        id="username"
+        name="username"
+        onChange={handleChange}
+      />
+      <Label htmlFor="password">Password</Label>
+      <Input
+        type="password"
+        id="password"
+        name="password"
+        onChange={handleChange}
+      />
+      <Label htmlFor="firstName">First Name</Label>
+      <Input
+        type="text"
+        id="firstName"
+        name="firstName"
+        onChange={handleChange}
+      />
+      <Label htmlFor="lastName">Last Name</Label>
+      <Input
+        type="text"
+        id="lastName"
+        name="lastName"
+        onChange={handleChange}
+      />
+      <Label htmlFor="email">Email</Label>
+      <Input
+        type="email"
+        id="email"
+        name="email"
+        onChange={handleChange}
+      />
+      <Button color="primary">Sign Up</Button>
     </Form>
   )
 
