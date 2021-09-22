2b4dfed21b65bcdf304837a39b7bdca13b1a6bdb
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-22 12:42:31 -0500

    added functionality for showing jobs

--- frontend/src/JobList.js
+++ frontend/src/JobList.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import JoblyApi from './api'
+import JobCard from './JobCard'
+import SearchForm from './SearchForm'
 
 const JobList = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+
+  const getJobs = async () => {
+    let comps = await JoblyApi.getAllJobs()
+    setJobs(comps)
+  }
+
+  const filterJobs = async (searchFilters) => {
+    setIsLoading(true)
+    let comps = await JoblyApi.getAllJobs(searchFilters)
+    setJobs(comps)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getJobs()
+    setIsLoading(false)
+  }, [])
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div>
-      
+      <SearchForm filter={filterJobs} searchType='jobs'/>
+      {jobs.map(j => (
+        <JobCard
+          key={j.id}
+          id={j.id}
+          title={j.title}
+          salary={j.salary}
+          equity={j.equity}
+        />
+      ))}
     </div>
   )
 }
